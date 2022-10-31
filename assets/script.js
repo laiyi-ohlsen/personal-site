b79245133df10959f0945748b1c8523e8c61f81a
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2022-10-30 22:47:24 -0400

    handlebars version

---
+++ assets/script.js
@@ -0,0 +1,179 @@
+
+/*----------DATA-------------*/
+
+/*var sample= {
+		title: 'TITLE',
+		sections: [{
+			title: '00 Title',
+			description:'00 Description'},{
+			title: '01 Title',
+			description: '01 Description'},{
+			title: '02 Title', 
+			description: '02 Description'},{
+			title: '03 Photos'
+		}], 
+		photos: [{
+			name: '[01]',
+			link: '../images/[IMAGE]'},{
+			name: '[02]',
+			link: '../images/[IMAGE]'}, {
+			name: '[03]',
+			link: '../images/[IMAGE]'}
+				]
+};
+
+*/
+
+var _100scores = {
+		title: '100 scores: movement inspired by computers',
+		sections: [{
+			title: ' 00 Questions',
+			description: '<i>100 Scores: movement inspired by computers</i> is collection of improvisational movement scores based upon computational algorithms. <i>000</i>, <i>001</i>, <i>010</i>, and </i>100</i> each focus upon a different theme: Networks, Organization, Timing and Trust, respectively. The scores are written to be danced, using the physical medium to translate computer theory to the physical body. '},{
+			title: '01 Publication',
+			description: "100 Scores was distributed as a zine at <a href='https://techzinefair.org/' target='_blank'>New York Tech Zine Fair 2018</a>, <a href='http://nyartbookfair.com/' target='_blank'>New York Art Book Fair 2018</a> and the Creative Independent / are.na <a href='https://thecreativeindependent.com/notes/arena-tci-how-do-you-use-the-internet-mindfully/' target='_blank'>How to Use the Internet Mindfully</a> release."},{
+			title: '02 Participatory ', 
+			description: "From 2016-2017, 100 Scores was developed into a series of particpatory performance of improvisational scores inspired by computers that have created a repository of responses. 100 Scores was presented in 5 cities including Berlin, Germany for Flee Immediately at Panke Gallery as part of <a href='https://vorspiel.berlin/#overlay=node/38145/modal' target='_blank'>Vorspiel,</a>&nbsp;Washington, D.C. at American University and Richmond, VA at Sediment Arts."}, {
+				title: '03 Photos'}
+				], 
+		photos: [{
+			name: '[01]',
+			link: '../images/100_scores_1.jpg'},{
+			name: '[02]',
+			link: '../images/100_scores_2.jpg'}, {
+			name: '[03]',
+			link: '../images/100_scores_3.jpg'}
+				]
+};
+
+
+var _while_x = {
+		title: 'while(x)',
+		sections: [{
+			title: '00 Background',
+			description: '<i>while(x)</i> is an reflective practice which considers code as a language for the logical processing of emotion. What are the constants? What are the variables?'},{
+			title: '01 Publication',
+			description: "0-9 was released in physical form July 2018 by <a href='http://silkhousepublishing.com/' target='_blank'>Silk House Publishing.</a> Available for download <a href='https://drive.google.com/drive/folders/1GddBA7MXirjXKA8oRGVub1r1Fb_4_sdW?usp=sharing' target='_blank'>here</a>."},{
+			title: '02 Participatory ', 
+			description: "The practice has been presented as a workshop by  Pioneer Works (<a href='https://pioneerworks.org/classes/whilex/' target='_blank'>[01]</a>, <a href='https://pioneerworks.org/programs/software-for-artists-day-5/' target='_blank'>[02]</a>, <a href='https://pioneerworks.org/classes/whilex-how-to-write-a-code-poem/' target='_blank'>[03]</a>) and <a href='https://rehearsal.cargo.site/events' target='_blank'>rehearsal residency</a>."},{
+			title: '03 Photos'
+		}], 
+		photos: [{
+			name: '[01]',
+			link: '../images/while-x_1.jpeg'},{
+			name: '[02]',
+			link: '../images/while-x_2.png'}
+				]
+};
+
+var _qlmQ = {
+		title: 'qlmQ',
+		sections: [{
+			title: '00 Description',
+			description:"From the Internet Archive's Deecntralized Web Summit Secrets Exhibition:</d> qlmQ subverts the use of an encrypted protection by exposing the artist’s private key on a common sticker. What is supposed to be a unique, secret identifier for the artist’s identity will be transposed onto a flexible, vulnerable, reproducible format designed for mass proliferation. The act of careless distribution positions privacy in a permanently public context and challenges the notion of their dichotomy. By sticking privacy onto public spaces globally, Ohlsen asks: what is the potential for a spectrum between these two poles?"},{
+			title: '01 Presentation',
+			description: "<i>qlmQ</i> was distributed at Internet Archive's <a href='https://decentralizedweb.net/' target='_blank'>Decentralized Web Summit</a> in August 2018 and presented at <a href='https://peer-to-peer-web.com/nyc' target='_blank'>Peer-to-Peer NYC</a> in May 2018 at School for Poetic Computation (video <a href='https://peer-to-peer-web.com/nyc' target='_blank'>here</a>) and 'Far / Near' Premiere in June 2018 at Mesa Arts Center, supported by <a href='http://www.nuebox.org/' target='_blank'>[nueBOX]</a>"},{
+			title: '03 Photos'
+		}], 
+		photos: [{
+			name: '[01]',
+			link: '../images/qlmq.jpg'
+		}]
+};
+
+var _cOfC = {
+		title: 'Choreography of Circulation',
+		sections: [{
+			title: '00 Questions',
+			description:"If, as Marshall Mcluhan famously said,“the medium is the message” then what can we learn from the mediated body? And if the body is being mediated by digital bits, is it more than a “remediation” of the technology we use to disseminate it? That is, at what point does the body’s ability to move as its own medium give way to its for-better-or-for-worse digital counterparts? What qualitative shifts occur when a body moves as a file, a fragment, a zero, or a one - and do they  even matter? When looking at the chunky, blurry version of reality that technology asks us to (literally) subscribe to, Hito Steyerl reminds us,“one could of course argue that this is not the real thing, but then—please, anybody—show me this real thing.”"},{
+			title: ' 01 Presentation',
+			description: "The <i>Choreography of Circulation</i> was performed through movement as part of <a href ='https://movementresearch.org/series/festival-fall-2018' target='_blank'>Movement Research's 2018 Fall Festival</a> and <a href='https://www.chezbushwick.net/air' target='_blank'>Jonah Boaker Arts Center's 2019 Artist-in-Residence</a> program."},{
+			title: ' 02 Performance', 
+			description: "While in residence at <a href='www.pioneerworks.org' target='_blank'>Pioneer Works</a>, I produced subsequent iterations of my moving body as it was uploaded and downloaded one thousand times through Instagram’s compression algorithm. I invite the audience to download, screenshot, upload, crop, zoom, trim, post and otherwise illicitly choreograph the image as their own. The full piece can be viewed <a href='https://www.instagram.com/choreographyofcirculation/' target='_blank'>here</a>."},{
+			title: '03 Conversation',
+			description: "Lai Yi Ohlsen and Marguerite Hemmings in conversation about the Choreography of Circulation. Edited excerpt on the Broadcast <a href='https://pioneerworks.org/broadcast/' target='_blank'>here</a> and full, unedited version <a href='https://docs.google.com/document/d/1REaC4TwJYYI2dkNGihtRWtrHdC5w2NQtkO4tdsBoHyw/edit?usp=sharing'>here</a>. I also spoke at length about the project (to myself) on Montez Press Radio in September 2020. You can listen to <a href='https://radio.montezpress.com/#/archive' target='_blank'>here</a> or email me for a the recording."
+		}]
+};
+
+var _timeSequence = {
+		title: 'Time Sequence Choreographies for a Room',
+		sections: [{
+			title: '00 Description',
+			description:"<b>From the Office Space 2 Press Release:</b> For Spatial Systems for Everyday Living, she presents <i>TIME SEQUENCE CHOREOGRAPHIES FOR A ROOM</i>, a series of twenty diagrammatic drawings that function as choreographic scores. Ohlsen’s simplified scores explore rhythm and movement through the concept of “sequence diagrams” in software engineering, which indicate how a message is sent and received. Additionally, the works take inspiration from early digital artist Manfred Mohr, who created some of the first algorithmically generated artworks in the 1960s. In collaboration with members of the audience, Ohlsen will perform a real-time translation of the drawings into movement on the exhibition’s opening night."},{
+			title: '01 Presentation',
+			description: "<i>TIME SEQUENCE CHOREOGRAPHIES FOR A ROOM</i> was shown as part of Office Space 2's Spatial Systems for Everyday Living in January 2020."},{
+			title: '02 Photos'
+		}], 
+		photos: [{
+			name: '[01]',
+			link: '../images/time-sequence_1.jpg'},{
+			name: '[02]',
+			link: '../images/time-sequence_2.jpg'}, {
+			name: '[03]',
+			link: '../images/time-sequence_3.jpg'}
+				]
+};
+
+var _database = {
+		title: 'Database as a Relational Practice',
+		sections: [{
+			title: '00 Description ',
+			description:'Database Design as a Relational Practice was a performative workshop given at Rehearsal residency in Long Island, New York. Participants were asked to use entity-relationship models, a foundation for database design, to outline a situation they considered to be difficult to resolve. Participants then used the design as a map for movement, by assigning a gesture to each box and a rhythm for each line. At the end of the workshop, participants moved through a map that they had no memory of making.'},{
+			title: '01 Video',
+			description: "<a href='https://vimeo.com/320798264' target='_blank'>[00]</a>"},{
+			title: '02 Photo'
+			}],
+			photos: [{
+			name: '[01]',
+			link: '../images/database_1.jpeg'},{
+			name: '[02]',
+			link: '../images/database_2.jpeg'}, {
+			name: '[03]',
+			link: '../images/database_3.jpeg'}
+				]
+};
+
+/*----------TEMPLATES-------------*/
+
+
+function fill_template(src) {
+	var template = Handlebars.templates['project-template'](src);
+	document.querySelector('#output').innerHTML = template;
+
+}
+
+
+
+/*----------PHOTOS-------------*/
+
+function show_image(src) {
+	var img = document.createElement('img');
+	img.src = src
+	img.setAttribute('class', 'responsive');
+	img.setAttribute('id', 'img');
+	
+	document.body.appendChild(img);
+}
+
+function remove_image() {
+	let images = document.getElementById('img');
+	console.log(images)
+	if (images != null){
+ 		images.remove()
+ 	}
+}
+
+function photos() {
+var links = document.querySelectorAll('a.image-link');
+
+for(let i=0; i < links.length; i++){
+    let url = links[i].dataset.url
+
+    links[i].addEventListener('click', function(){
+    	remove_image();
+    	show_image(url)
+    }, false);
+   
+}}
+
+
+
