dbdb1943f86f98291e6a0560b90b5f1ecf68a75e
Lai Yi Ohlsen <laiyi.ohlsen@gmail.com>
2022-10-30 23:15:27 -0400

    add the Internet

--- assets/script.js
+++ assets/script.js
@@ -132,6 +132,23 @@ var _database = {
 				]
 };
 
+var _theInternet= {
+		title: '\"the Internet\"',
+		sections: [{
+			title: '00 Description',
+			description:"Taking inspiration from the narrative diagrams of neo—conceptual artist Mark Lombardi, Ohlsen moves between diagram and drawing to connect various actors, concepts, and institutions who are all known to have an influence over the internet, and yet are rarely associated with one another. Drawing upon her experiences as a technologist, artist, and researcher within a diverse set of Internet—focused ecosystems, she places these dimensions on the same plane to consider: who has influence over the Internet? How are these influences related? How does power flow between these nodes and who do we need to involve if we want to reimagine its shape?"},{
+			title: '01 Dimensions',
+			description: 'Ink on paper; 18" x 24"'},{
+			title: '02 Presentation', 
+			description: '\"The Internet\" was shown at <a href = "LINK">Public Works Administration</a> for the Year 8  New Inc Art + Code Track End of Year Show, organized by Celine Katzman & Rhizome.'},{
+			title: '03 Photos'
+		}], 
+		photos: [{
+			name: '[01]',
+			link: '../images/internet_1.jpeg'}
+				]
+};
+
 /*----------TEMPLATES-------------*/
 
 
